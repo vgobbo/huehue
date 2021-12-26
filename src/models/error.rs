5263226104bd90e529c1901f631b1263ab910b63
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2021-12-27 00:16:09 +0100

    Added Hue error definitions (not extensive).

---
+++ src/models/error.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
+
+#[derive(Debug, Clone, Eq, PartialEq, Serialize_repr, Deserialize_repr)]
+#[repr(u32)]
+pub enum ErrorCode {
+	Unauthorized = 1,
+	InvalidJson = 2,
+	ResourceNotAvailable = 3,
+	ResourceMethodNotAvailable = 4,
+	ParameterMissing = 5,
+	ParameterNotAvailable = 6,
+	ParameterValue = 7,
+	ParameterNotModifiable = 8,
+	TooManyItems = 11,
+	PortalConnectionRequired = 12,
+	InternalError = 901,
+
+	LinkButtonNotPressed = 101,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Error {
+	#[serde(rename = "type")]
+	pub r#type: ErrorCode,
+
+	pub address: String,
+	pub description: String,
+}
