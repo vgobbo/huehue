e01f210c016abb2f7a29bc0af2838c5e7a3e023d
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2021-12-27 00:17:11 +0100

    Derive from Clone.

--- src/models/device_type.rs
+++ src/models/device_type.rs
@@ -11,7 +11,7 @@ pub enum Error {
 }
 
 /// Represents the weirdly named `devicetype`, used as identification during authentication.
-#[derive(Serialize, Deserialize)]
+#[derive(Clone, Serialize, Deserialize)]
 pub struct DeviceType {
 	pub application_name: String,
 	pub device_name: String,
