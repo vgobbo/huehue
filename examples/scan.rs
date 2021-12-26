673eb27f3ba4afc3ba65bdee334cba5fb0e956c1
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2021-12-27 00:44:59 +0100

    Scan example.

---
+++ examples/scan.rs
@@ -0,0 +1,22 @@
+use std::time::Duration;
+
+use rues::Bridge;
+
+#[tokio::main]
+async fn main() {
+	println!("Scanning for bridges for 5 seconds.");
+	let bridges = Bridge::discover(Duration::from_secs(5)).await;
+	println!("{} bridges found.\n", bridges.len());
+
+	let mut i = 1;
+	for bridge in &bridges {
+		println!("> Bridge #{}:", i);
+		i += 1;
+
+		println!("\tIdentifier: {}", bridge.id);
+		println!("\tModel: {:?}", bridge.model);
+		println!("\tVersion: {}", bridge.version);
+		println!("\tAddress: {}", bridge.address);
+		println!("\tSupported: {}", bridge.supported);
+	}
+}
