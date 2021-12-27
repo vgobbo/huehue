f9016e91fb5018302b98c9d7f35b05dad8d748ef
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2021-12-28 00:07:53 +0100

    API get lights models.

---
+++ src/models/lights.rs
@@ -0,0 +1,37 @@
+use serde::{Deserialize, Serialize};
+
+use crate::models::device_type::DeviceType;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Metadata {
+	pub archetype: String,
+	pub name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct On {
+	pub on: bool,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct Dimming {
+	pub brightness: f32,
+	pub min_dim_level: f32,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetLightsResponse {
+	pub data: Option<Vec<GetLightsResponseItem>>,
+	pub error: Option<crate::models::Error>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GetLightsResponseItem {
+	#[serde(rename = "type")]
+	pub r#type: String,
+
+	pub id: uuid::Uuid,
+	pub metadata: Metadata,
+	pub dimming: Dimming,
+	pub on: On,
+}
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -2,6 +2,7 @@ pub mod config;
 pub mod create_user;
 pub mod device_type;
 pub mod error;
+pub mod lights;
 pub mod meethue;
 
 pub use config::Config;
