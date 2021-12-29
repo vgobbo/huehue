297a81fa5220c0b110b7ffbfec05ed4cfe61146d
Vinicius Gobbo Antunes de Oliveira <vgobbo@gmail.com>
2021-12-29 01:32:47 +0100

    Experimental light switch.

--- src/light.rs
+++ src/light.rs
@@ -1,6 +1,8 @@
 use crate::color::{Color, Temperature};
-use crate::models::lights::GetLightsResponseItem;
-use crate::Client;
+use crate::http::HueError;
+use crate::models::lights::{GetLightsResponseItem, LightOnRequest};
+use crate::models::GenericResponse;
+use crate::{http, Client};
 
 pub type Lights = Vec<Light>;
 
@@ -25,6 +27,23 @@ impl Light {
 			brightness: light.dimming.brightness,
 			color: light.color,
 			temperature: light.color_temperature,
+		}
+	}
+
+	pub async fn switch(&mut self, on: bool) -> Result<(), HueError> {
+		let url = self
+			.client
+			.bridge()
+			.url(format!("clip/v2/resource/light/{}", self.id).as_str());
+		let application_key = self.client.application_key().clone().unwrap();
+		let request_payload = LightOnRequest::new(on);
+
+		match http::put_auth::<GenericResponse, LightOnRequest>(application_key, url, &request_payload).await {
+			Ok(_) => {
+				self.on = on;
+				Ok(())
+			},
+			Err(e) => Err(e),
 		}
 	}
 }
